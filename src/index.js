59311f53d1a23df4f5590a8c9c09a61fe5ef54d3
yannnaaa <yannnaaa@mail.ru>
2019-10-08 22:30:23 +0300

    add function brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,48 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arr = [];
+  let arrStr;
+  
+  arrStr = str.split('');
+  
+  for (let i = 0; i < arrStr.length; i++ ) {
+    
+    for (let j = 0; j < bracketsConfig.length; j++ ) {
+
+      if ( arrStr[i] === bracketsConfig[j][0] ) { 
+	    arr.push([arrStr[i],j]);                  	    
+      }
+    }     
+	
+    for (let j = 0; j < bracketsConfig.length; j++ ) {     
+      if ( ( arrStr[i] === bracketsConfig[j][1] ) 
+        && ( arrStr[i] !== bracketsConfig[j][0] ) ) { 
+	     
+	if ( arr.length > 0 ) {
+		 
+          if ( arr[arr.length - 1][1] === j ) { 
+	    arr.pop(); 
+	  } else {
+	    return false;
+	  }
+		         	    
+	} else {
+	  return false;
+	}	
+      } 
+	  
+      if ( ( arrStr[i] === bracketsConfig[j][1] ) 
+        && ( arrStr[i] === bracketsConfig[j][0] ) ) {
+
+	let count = arr.reduce( (s,a,i,m) => 
+          a[0] === bracketsConfig[j][1] ? s+1 : 0, 0 );    
+	    
+        if ( count % 2 === 0 ) {
+	  arr.pop();
+	  arr.pop();
+	}    
+      }	  		  	     	 
+    }  
+  }  
+  
+  return arr.length === 0 ? true : false ;
 }
